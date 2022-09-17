e7752580a26e31abf393a61936db22ddbe037f9d
flexyaun <sreyasr7@gmail.com>
2022-09-17 20:48:35 +0530

    added axios

--- build/index.js
+++ build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('Hello world!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+axios_1.default
+    .get('https://example.com/todos')
+    .then(function (res) {
+    console.log("statusCode: ".concat(res.status));
+    console.log(res);
+})
+    .catch(function (error) {
+    console.error(error);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import axios from 'axios';
+
+axios
+  .get('https://example.com/todos')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
